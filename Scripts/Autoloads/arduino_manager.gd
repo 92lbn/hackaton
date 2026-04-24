5914c23e191cc23503ed573924cd88b7992177ba
ZaidBelkacem <zaid.belkacem03@gmail.com>
2026-04-24 10:10:58 +0200

    ajout des dernier song et modification player

--- Scripts/Autoloads/arduino_manager.gd
+++ Scripts/Autoloads/arduino_manager.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 	serial = GdSerial.new()
 	serial.set_baud_rate(BAUD_RATE)
 	serial.set_timeout(10)
-	start_pairing()
+	start_pairing()	
 	if serial.open():
 		_paired = true
 		_wasPaired = true
